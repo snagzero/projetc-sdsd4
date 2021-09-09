a64215684a8b8fabe5973028d5a812eba4cf284d
Pedro Esperança <snagzero@gmail.com>
2021-09-09 17:44:41 -0300

    layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -16,7 +16,7 @@ public class Seller {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	
 	@OneToMany(mappedBy = "seller")
